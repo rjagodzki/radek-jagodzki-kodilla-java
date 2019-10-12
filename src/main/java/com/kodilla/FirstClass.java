c42e3fb1a80f911340eea9c9a545f5d535ebaf88
rjagodzki <jagodzki.radek@outlook.com>
2019-10-12 22:14:03 +0200

    Second commit

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,9 @@
 package com.kodilla;
 
 public class FirstClass {
+    public static void main(String args[]){
+        System.out.println("Dziala?");
+        int a = 5 , b = 6 ;
+        System.out.print(a+b);
+    }
 }
