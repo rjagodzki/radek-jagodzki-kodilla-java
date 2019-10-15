fb664a3c8e5a9b313de3f91d2a252a722bcf48b0
rjagodzki <jagodzki.radek@outlook.com>
2019-10-16 01:03:52 +0200

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Kolejny test
+        System.out.println("Test - pierwszy test jednostkowy: ");
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("Dodawanie: " + calculator.add(5,10));
+        System.out.println("Odejmowanie: " +calculator.substract(10,9));
+
     }
 }
