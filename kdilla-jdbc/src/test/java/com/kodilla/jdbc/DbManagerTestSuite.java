8199da2fffa6a66eb04e1e9d51c6e8843ada3206
rjagodzki <jagodzki.radek@outlook.com>
2019-11-25 20:53:31 +0100

    modul 16.6

---
+++ kdilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,69 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException{
+        //Given
+        //When
+        DBManager dbManager = DBManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException{
+        //Given
+        DBManager dbManager = DBManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()){
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+
+        Assert.assertEquals(5,counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException{
+        //Given
+        DBManager dbManager = DBManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS NUMBER_OF_POSTS " +
+                          "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID " +
+                          "GROUP BY U.ID " +
+                          "HAVING COUNT(*)>=2";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()){
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME") + ", " +
+                    rs.getString("NUMBER_OF_POSTS"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+
+        Assert.assertEquals(1,counter);
+    }
+}
